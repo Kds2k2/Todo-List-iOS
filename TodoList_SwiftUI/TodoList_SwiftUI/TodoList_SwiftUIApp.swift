544aa5e0afe70635386163db8108e285803cb2a6
Dima Kryzhanovsky <dimakryzha@gmail.com>
2024-05-27 19:09:22 +0300

    Initial commit. Create projects for UIKit & SwiftUI.

---
+++ TodoList_SwiftUI/TodoList_SwiftUI/TodoList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_SwiftUIApp.swift
+//  TodoList_SwiftUI
+//
+//  Created by Dmitro Kryzhanovsky on 27.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
